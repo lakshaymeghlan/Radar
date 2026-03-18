b8b07543e6a551d4820d163734d2c27c8bdc06d3
Lakshay Meghlan <lakshay.meghlan@techchefz.com>
2026-03-18 23:56:50 +0530

    feat: Implement API route to fetch user profile by ID and their associated startups.

--- app/api/profile/[id]/route.ts
+++ app/api/profile/[id]/route.ts
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
